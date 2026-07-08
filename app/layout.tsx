a8afa7d9a0187135fc902fa5196d7230580386d6
hideaki-yawata <hideaki.yawata@gmail.com>
2026-07-08 20:30:01 +0200

    10章まで完了

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,25 @@
 import "./globals.css";
+import { GoogleAnalytics } from "@next/third-parties/google";
+import type { Metadata } from "next";
 import Header from "./_components/Header";
 import Footer from "./_components/Footer";
+
+export const metadata: Metadata = {
+  metadataBase: new URL("http://localhost:3000"),
+  title: {
+    template: "%s | シンプルなコーポレートサイト",
+    default: "シンプルなコーポレートサイト",
+  },
+  description: "「Next.js+ヘッドレスcmsではじめる！かんたん・モダンWebサイト制作入門」で作成されるサイトです。",
+  openGraph: {
+    title: "シンプルなコーポレートサイト",
+    description: "「Next.js+ヘッドレスcmsではじめる！かんたん・モダンWebサイト制作入門」で作成されるサイトです。",
+    images: ["/ogp.png"],
+  },
+  alternates: {
+    canonical: "http://localhost:3000",
+  },
+};
 
 export default function RootLayout({
   children,
@@ -14,6 +33,7 @@ export default function RootLayout({
         {children}
         <Footer />
       </body>
+      <GoogleAnalytics gaId="G-G2FF1F9Q23" />
     </html>
   );
 }
